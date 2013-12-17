1ec821ab1c012ccd360e57b8fd79d72df862db1f
Bigfa <bigfa@pku.edu.cn>
2013-12-17 21:30:24 +0800

    fixed ajax comment bug
    fixed ajax comment bug

--- presscore-lite/js/single.js
+++ presscore-lite/js/single.js
@@ -1,1 +1,350 @@
-eval(function(p,a,c,k,e,d){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--){d[e(c)]=k[c]||e(c)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('2c(G).2e(3(a){L(3n){3 v(){$1O.1m(),$1S.1m(),a("32").1N(3(){F.H=""}),o=""}3 y(){w>0?(u.1D(w),w--,2L(y,2B)):(u.1D(x).E("1o",!1).1t("1V",1),w=15)}A o,w,x,b=a("#1z"),c="0",d=b.E("1X").19("O-1Y-2i.3l",""),e=d+"O-1P/3z/3t-3u/W/3m.2x",f=d+"O-2N/2O/3x.1Q",g=d+"O-2N/2O/3w.1Q",h=\'<7 9="V-4-16"><W 1j="\'+e+\'" B="29-1p:1M;" 1K=""/> | 真的是V提交~</7>\',i=\'<7 9="18">#</7>\',j=\'"><W 1j="\'+g+\'" B="29-1p:1M;" 1K=""/> 提交成功\',k=\', 刷新页面之前可以<a 3q="3p" 1R="4-T-1d" 13="#3s" 1u=\\\'K 1s.2p("\',l=")\'>再编辑</a>",m="取消编辑",p=0,q=[],r=a("#1Y-2W"),s=a("#17-4-T-1d"),t=s.P(),u=a("#1z #2V");u.E("1o",!1),$J=1l.42?"3U"==G.3T?a("14"):a("J"):a("14,J"),a("#T-2W").39(h+i),a("#V-4-16").S(),a("#18").S(),b.2V(3(){K a("#1z,#Z 7.17-4-T,#Z 33").S(),a("#V-4-16").2S(),u.E("1o",!0).1t("1V",.5),o&&a("#4").3S(\'<3R 30="P" 3V="1J" 9="1J" H="\'+o+\'" B="X:11;" />\'),a.V({2l:d,3W:a(F).41()+"&1X=40",30:a(F).E("3B"),18:3(b){a("#V-4-16").2Q(),a("#18").2S().14(\'<W 1j="\'+f+\'" B="29-1p:1M;" 1K=""/> \'+b.2n),2L(3(){u.E("1o",!1).1t("1V",1),a("#18").2Q()},3Z)},2h:3(b){a("#V-4-16").S(),a("#3X").3Y(),a("#1z,#Z 7.17-4-T,#Z 33").1m(),q.3O(a("#4").1D()),a("32").1N(3(){F.H=""});A d=1s,e=d.I("17-4-T-1d"),f=d.I("O-M-1k-7"),g=d.I(d.1I),h=d.I("2f").H,i=d.I("1f").H;!o&&r.2g&&(n=3G(r.P().2m(/\\d+/)),r.P(r.P().19(n,n+1))),1h=\'" 9="Y\'+p+\'"></\',1h="0"==i?\'\\n<3k B="3E:3C;" 1R="3D\'+1h+"3k>":\'\\n<37 1R="3N\'+1h+"37>",1n=\'\\n<1C 9="2A\'+p+j,"1"==c&&(3f=-1==G.J.1a.36("7-4-")?"":-1==G.J.1a.36("3c-4-")?"7-":"",1n=1n.3M(k,3f,"4-",i,\'", "\',i,\'", "Z", "\',h,\'", \',p,l)),1n+="</1C><1C></1C>\\n",a("#Z").39(1h),a("#Y"+p).S().1q(b),a("#Y"+p+" 3c").1q(1n),a("#Y"+p).2U(3K),$J.20({2d:a("#Y"+p).26().Q-3J},3L),y(),p++,o="",a("*").1G("#1J"),e.B.X="11",e.1u=2v,d.I("1f").H="0",f&&g&&(f.R.1e(g,f),f.R.1Z(f))}}),!1}),1s={2p:3(b,c,d,e,f){A h,g=F,i=g.I(b),j=g.I(d),k=g.I("17-4-T-1d"),l=g.I("1f"),n=g.I("2f");o&&v(),q.2g>0&&f?(g.I("4").H=q[f],o=g.I("Y"+f).1a.2m(/(4-)(\\d+)/)[2],$1S=a("#2A"+f),$1S.S(),$1O=a("#Y"+f),$1O.S(),s.P(m)):s.P(t),g.1I=d,e=e||!1,g.I("O-M-1k-7")||(h=G.3H("7"),h.9="O-M-1k-7",h.B.X="11",j.R.1e(h,j)),i?i.R.1e(j,i.3F):(M=g.I("O-M-1k-7"),g.I("1f").H="0",M.R.1e(j,M),M.R.1Z(M)),$J.20({2d:a("#Z").26().Q-3P},2X),n&&e&&(n.H=e),l.H=c,k.B.X="",k.1u=3(){o&&v();A a=1s,b=a.I("O-M-1k-7"),c=a.I(a.1I);K a.I("1f").H="0",b&&c&&(b.R.1e(c,b),b.R.1Z(b)),F.B.X="11",F.1u=2v,!1};3Q{g.I("4").2t()}3r(p){}K!1},I:3(a){K G.2q(a)}},w=15,x=u.1D()}});2c(G).2e(3(a){A l=a(".3o"),y=3y,r=3A,z=\'<7 9="16-1Y"></7>\';l.3v("10",".3I a",3(b){b.4n();A b=a(F).E("13"),c=1,j=a("#17-4-T-1d");/4-21-/i.2k(b)?c=b.1c(/4-21-/i)[1].1c(/(\\/|#|&).*$/)[0]:/2j=/i.2k(b) && (c = b.1c(/2j=/)[1].1c(/(\\/|#|&).*$/)[0]);j.10();a.V({2l:r+"?1X=4U&2i="+y+"&21="+c,4T:3(){l.14(z)},18:3(a){2G(a.2n)},2h:3(b){l.14(b);a("J, 14").20({2d:l.26().Q-50},2B)}})})});(3(){3 $(9){K G.2q(9)}3 2a(22,2E){A 24=$(22).1a.19(/<.+?>/4V,"").19(/\\t|\\n|\\r\\n/g,"");A 4=$(2E).1a;$("4").H+=\'<2s><2y><a 13="#4-\'+22.19(/24-/,"")+\'">\'+24+"</a> :</2y>"+4.19(/\\t/g,"")+"</2s>";$("4").2t()}1l["2u"]={};1l["2u"]["2a"]=2a})();2c(G).2e(3(){4W="2w:2Z;1F:1U;Q:50%;28:50%;1g-Q:-1L;1g-28:-1L;1y:2b;12:2b;3b-12:2b;P-1p:51;1T-34:4X;2C-1x:#4S;1x:#4R;2F-4M:4L;";A d=0;$("#1P .4N").1N(3(){A a=$(F).E("13").4O();A b=a.4Q(a.4P("."));L(b==".43"||b==".4Z"||b==".1Q"||b==".2x"||b==".55"){$(F).5a("U-2D").E("9","U-"+d);d++}});$("#1P a.U-2D").10(3(){A a=$(F).E("9").1c(/U-/)[1],1W=3g(),1r=$(F).E("13"),2J="z-2z:59;57:53;1F:1U;1B:0;Q:0;1y:1H%;12:1H%;2C-1x:#38;",2K="z-2z:52;1F:1U;1B:50%;Q:50%;",25="X:11;2F:58 54 #56;4Y-3d:2o 2o 4J 4i(0,0,0,0.6)",27="X:11;2w:2Z;1F:4h;Q:50%;3b-12:1L;1g:-35 0 0 0;1x:#4g;P-3d:4f 4j 4k #38;1T-34:35;1T-4K:4m,4l;";L(4e G.J.B.4d==="47"){2G("\\46\\45\\44\\48\\49\\4c\\4b\\4a\\4o\\2H\\2P\\2M\\4p\\4D\\4C\\2P\\4B\\4E\\2M\\4F\\4I\\4H\\4G\\2H\\2I\\2I");K 1E}23{$("J").1q(\'<7 9="1b" B="\'+2J+\'"></7><p 9="1v"></p><7 9="U" B="\'+2K+\'"><W 9="N" B="\'+25+\'" /><p 9="1i" B="1B:-2R;\'+27+\'">&4A;</p><p 9="1A" B="28:-2R;\'+27+\'">&4z;</p></7>\');$("#1b").1t(2T,.8);$1v=$("#1v");N("#N",1r,1W,a,d);$("#1i,#1A").10(3(){L($(F).E("9")=="1i")a--;23 a++;$("#U").4t("W").1G().4s().1q(\'<W 9="N" B="\'+25+\'" />\');1r=$("#U-"+a).E("13");N("#N",1r,1W,a,d);K 1E});$("#1b,#N").10(3(){$1v.1G();$("#1b,#N").4r("10");$("#1b,#U").4q(2X,3(){$(F).1G()});K 1E})}K 1E});3 N(b,c,d,e,f){$("#1i,#1A").S();A g=4u 4v();g.4y=3(){A x=d[0]-1H,y=d[1]-1H,C=g.1y,D=g.12;L(C>x){D=D*(x/C);C=x;L(D>y){C=C*(y/D);D=y}}23 L(D>y){C=C*(y/D);D=y;L(C>x){D=D*(x/C);C=x}}A a=-(C/2+5)+"1w",31=-(D/2+5)+"1w";C=C+"1w",D=D+"1w";$(b).E("1j",c).2Y({1y:C,12:D}).2U(2T).4x().2Y({"1g-1B":a,"1g-Q":31});L(e>0)$("#1i").1m();L(e<f-1)$("#1A").1m()};g.1j=c}3 3g(){A a=G.4w;A w=1l.3h||3j.3h||a&&a.3i||G.J.3i;A h=1l.3e||3j.3e||a&&a.3a||G.J.3a;2r=[w,h];K 2r}});',62,321,'|||function|comment|||div||id|||||||||||||||||||||||||||var|style|img_w|img_h|attr|this|document|value||body|return|if|temp|bigfa_img|wp|text|top|parentNode|hide|reply|bigfa|ajax|img|display|new_comm_|respond|click|none|height|href|html||loading|cancel|error|replace|innerHTML|bigfa_bg|split|link|insertBefore|comment_parent|margin|new_htm|bigfa_prev|src|form|window|show|ok_htm|disabled|align|append|bigfa_img_url|addComment|fadeTo|onclick|zshowbox_loading|px|color|width|commentform|bigfa_next|left|span|val|false|position|remove|100|respondId|edit_id|alt|80px|middle|each|new_comm|content|png|class|new_sucs|font|fixed|slow|pagesize|action|comments|removeChild|animate|page|authorId|else|author|css_bigfa_img|offset|css_bigfa_p_n|right|vertical|quote|160px|jQuery|scrollTop|ready|comment_post_ID|length|success|post|cpage|test|url|match|responseText|0px|moveForm|getElementById|arrayPageSize|blockquote|focus|SIMPALED|null|cursor|gif|strong|index|success_|1e3|background|showbox|commentBodyId|border|alert|u5427|u2026|css_bigfa_bg|css_bigfa|setTimeout|u5B83|admin|images|uFF0C|slideUp|30px|slideDown|600|fadeIn|submit|title|400|css|pointer|type|margintop|textarea|h2|size|40px|indexOf|ul|000|before|clientHeight|line|li|shadow|innerHeight|div_|bigfa_getPageSize|innerWidth|clientWidth|self|ol|php|ajaxload|comments_open|commentshow|nofollow|rel|catch|edit|presscore|lite|on|yes|no|fspostid|themes|fsajaxurl|method|both|commentlist|clear|nextSibling|parseInt|createElement|commentnav|200|4e3|900|concat|children|push|180|try|input|after|compatMode|CSS1Compat|name|data|commentPreview|empty|3e3|ajax_comment|serialize|opera|jpeg|u6301IE6|u652F|u4E0D|undefined|uFF01|u8BF7|u653E|u4EEC|u4F60|maxHeight|typeof|1px|eee|absolute|rgba|3px|5px|Tahoma|Arial|preventDefault|u8FC7IE6|u592A|fadeOut|unbind|end|find|new|Image|documentElement|parent|onload|raquo|laquo|u5C31|u4E86|u8001|u8BA9|u5B89|u53BB|u7684|u5FC3|10px|family|100px|radius|fancybox|toLowerCase|lastIndexOf|substring|999|666|beforeSend|fs_ajax_pagenavi|gim|zl_style_1|18px|box|jpg||center|99999|hidden|solid|bmp|fff|overflow|8px|9999|addClass'.split('|'),0,{}))
+/**
+ * WordPress jQuery-Ajax-Comments v1.0 by Bigfa.
+ * URI: http://fatesinger.com/jquery-ajax-comments
+ * Thanks Willin Kan
+ * Require Jquery 1.7+
+ */
+jQuery(document).ready(function($) {
+	var $commentform = $('#commentform'),		
+		txt1 = '<div id="loading">正在提交, 请稍候...</div>',
+		txt2 = '<div id="error">#</div>',
+		txt3 = '">提交成功', 
+		num = 0,
+		comm_array =[],
+		$comments = $('#comments-title'),
+		$cancel = $('#cancel-comment-reply-link'),
+		cancel_text = $cancel.text(),
+		$submit = $('#commentform #submit'); $submit.attr('disabled', false),
+		$body = (window.opera) ? (document.compatMode == "CSS1Compat" ? $('html') : $('body')) : $('html,body');
+	$('#comment').after( txt1 + txt2 ); $('#loading').hide(); $('#error').hide();
+	
+	/** submit */
+	$(document).on("submit", "#commentform",
+	function() {
+		editcode();
+		$('#loading').slideDown();
+		$submit.attr('disabled', true).fadeTo('slow', 0.5);
+		/** Ajax */
+		$.ajax( {
+			url: O_Connor.um_ajaxurl,
+			data: $(this).serialize() + "&action=ajax_comment",
+			type: $(this).attr('method'),
+
+			error: function(request) {
+				$('#loading').slideUp();
+				$('#error').slideDown().html(request.responseText);
+				setTimeout(function() {$submit.attr('disabled', false).fadeTo('slow', 1); $('#error').slideUp();}, 3000);
+				},
+
+			success: function(data) {
+				
+				$('#loading').hide();
+				comm_array.push($('#comment').val());
+				$('textarea').each(function() {this.value = ''});
+				var t = addComment, cancel = t.I('cancel-comment-reply-link'), temp = t.I('wp-temp-form-div'), respond = t.I(t.respondId), post = t.I('comment_post_ID').value, parent = t.I('comment_parent').value;
+
+				// comments
+				if ( $comments.length ) {
+					n = parseInt($comments.text().match(/\d+/));
+					$comments.text($comments.text().replace( n, n + 1 ));
+				}
+
+				// show comment
+				new_htm = '" id="new_comm_' + num + '"></';
+				new_htm = ( parent == '0' ) ? ('\n<ol style="clear:both;" class="commentlist' + new_htm + 'ol>') : ('\n<ul class="children' + new_htm + 'ul>');
+
+				ok_htm = '\n<span id="success_' + num + txt3;
+				ok_htm += '</span><span></span>\n';
+
+				$('#respond').before(new_htm);
+				$('#new_comm_' + num).hide().append(data);
+				$('#new_comm_' + num + ' li').append(ok_htm);
+				$('#new_comm_' + num).fadeIn(4000);
+
+				$body.animate( { scrollTop: $('#new_comm_' + num).offset().top - 200}, 900);
+				countdown(); num++ ;
+				cancel.style.display = 'none';
+				cancel.onclick = null;
+				t.I('comment_parent').value = '0';
+				if ( temp && respond ) {
+					temp.parentNode.insertBefore(respond, temp);
+					temp.parentNode.removeChild(temp)
+				}
+			}
+		}); // end Ajax
+		return false;
+	}); // end submit
+
+	/** comment-reply.dev.js */
+	addComment = {
+		moveForm : function(commId, parentId, respondId, postId, num) {
+			var t = this, div, comm = t.I(commId), respond = t.I(respondId), cancel = t.I('cancel-comment-reply-link'), parent = t.I('comment_parent'), post = t.I('comment_post_ID');
+			t.respondId = respondId;
+			postId = postId || false;
+
+			if ( !t.I('wp-temp-form-div') ) {
+				div = document.createElement('div');
+				div.id = 'wp-temp-form-div';
+				div.style.display = 'none';
+				respond.parentNode.insertBefore(div, respond)
+			}
+
+			!comm ? ( 
+				temp = t.I('wp-temp-form-div'),
+				t.I('comment_parent').value = '0',
+				temp.parentNode.insertBefore(respond, temp),
+				temp.parentNode.removeChild(temp)
+			) : comm.parentNode.insertBefore(respond, comm.nextSibling);
+
+			$body.animate( { scrollTop: $('#respond').offset().top - 180 }, 400);
+
+			if ( post && postId ) post.value = postId;
+			parent.value = parentId;
+			cancel.style.display = '';
+
+			cancel.onclick = function() {
+				var t = addComment, temp = t.I('wp-temp-form-div'), respond = t.I(t.respondId);
+
+				t.I('comment_parent').value = '0';
+				if ( temp && respond ) {
+					temp.parentNode.insertBefore(respond, temp);
+					temp.parentNode.removeChild(temp);
+				}
+				this.style.display = 'none';
+				this.onclick = null;
+				return false;
+			};
+
+			try { t.I('comment').focus(); }
+			catch(e) {}
+
+			return false;
+		},
+
+		I : function(e) {
+			return document.getElementById(e);
+		}
+	}; // end addComment
+
+	var wait = 15, submit_val = $submit.val();
+	function countdown() {
+		if ( wait > 0 ) {
+			$submit.val(wait); wait--; setTimeout(countdown, 1000);
+		} else {
+			$submit.val(submit_val).attr('disabled', false).fadeTo('slow', 1);
+			wait = 15;
+	  }
+	}
+function editcode() {
+	var a = "",
+	b = $("#comment").val(),
+	start = b.indexOf("<code>"),
+	end = b.indexOf("</code>");
+	if (start > -1 && end > -1 && start < end) {
+		a = "";
+		while (end != -1) {
+			a += b.substring(0, start + 6) + b.substring(start + 6, end).replace(/<(?=[^>]*?>)/gi, "&lt;").replace(/>/gi, "&gt;");
+			b = b.substring(end + 7, b.length);
+			start = b.indexOf("<code>") == -1 ? -6: b.indexOf("<code>");
+			end = b.indexOf("</code>");
+			if (end == -1) {
+				a += "</code>" + b;
+				$("#comment").val(a)
+			} else if (start == -6) {
+				myFielde += "&lt;/code&gt;"
+			} else {
+				a += "</code>"
+			}
+		}
+	}
+	var b = a ? a: $("#comment").val(),
+	a = "",
+	start = b.indexOf("<pre>"),
+	end = b.indexOf("</pre>");
+	if (start > -1 && end > -1 && start < end) {
+		a = a
+	} else return;
+	while (end != -1) {
+		a += b.substring(0, start + 5) + b.substring(start + 5, end).replace(/<(?=[^>]*?>)/gi, "&lt;").replace(/>/gi, "&gt;");
+		b = b.substring(end + 6, b.length);
+		start = b.indexOf("<pre>") == -1 ? -5: b.indexOf("<pre>");
+		end = b.indexOf("</pre>");
+		if (end == -1) {
+			a += "</pre>" + b;
+			$("#comment").val(a)
+		} else if (start == -5) {
+			myFielde += "&lt;/pre&gt;"
+		} else {
+			a += "</pre>"
+		}
+	}
+}
+function grin(a) {
+	var b;
+	a = " " + a + " ";
+	if (document.getElementById("comment") && document.getElementById("comment").type == "textarea") {
+		b = document.getElementById("comment")
+	} else {
+		return false
+	}
+	if (document.selection) {
+		b.focus();
+		sel = document.selection.createRange();
+		sel.text = a;
+		b.focus()
+	} else if (b.selectionStart || b.selectionStart == "0") {
+		var c = b.selectionStart;
+		var d = b.selectionEnd;
+		var e = d;
+		b.value = b.value.substring(0, c) + a + b.value.substring(d, b.value.length);
+		e += a.length;
+		b.focus();
+		b.selectionStart = e;
+		b.selectionEnd = e
+	} else {
+		b.value += a;
+		b.focus()
+	}
+}
+
+});
+
+jQuery(document).ready(function(a) {
+	var l = a(".commentshow"),
+	y = fspostid,
+	r = fsajaxurl,
+	z = '<div id="loading-comments"></div>';
+	l.on("click", ".commentnav a",
+	function(b) {
+		b.preventDefault();
+		var b = a(this).attr("href"),
+		c = 1,
+		j = a("#cancel-comment-reply-link");
+		/comment-page-/i.test(b) ? c = b.split(/comment-page-/i)[1].split(/(\/|#|&).*$/)[0] : /cpage=/i.test(b) && (c = b.split(/cpage=/)[1].split(/(\/|#|&).*$/)[0]);
+		j.click();
+		a.ajax({
+			url: r + "?action=fs_ajax_pagenavi&post=" + y + "&page=" + c,
+			beforeSend: function() {
+				l.html(z)
+			},
+			error: function(a) {
+				alert(a.responseText)
+			},
+			success: function(b) {
+				l.html(b);
+				a("body, html").animate({
+					scrollTop: l.offset().top - 50
+				},
+				1e3)
+			}
+		})
+	})
+}); (function() {
+	function $(id) {
+		return document.getElementById(id)
+	}
+	function quote(authorId, commentBodyId) {
+		var author = $(authorId).innerHTML.replace(/<.+?>/gim, "").replace(/\t|\n|\r\n/g, "");
+		var comment = $(commentBodyId).innerHTML;
+		$("comment").value += '<blockquote><strong><a href="#comment-' + authorId.replace(/author-/, "") + '">' + author + "</a> :</strong>" + comment.replace(/\t/g, "") + "</blockquote>";
+		$("comment").focus()
+	}
+	window["SIMPALED"] = {};
+	window["SIMPALED"]["quote"] = quote
+})();
+jQuery(document).ready(function() {
+	zl_style_1 = "cursor:pointer;position:fixed;top:50%;right:50%;margin-top:-80px;margin-right:-80px;width:160px;height:160px;line-height:160px;text-align:center;font-size:18px;background-color:#666;color:#999;border-radius:100px;";
+	var d = 0;
+	$("#content .fancybox").each(function() {
+		var a = $(this).attr("href").toLowerCase();
+		var b = a.substring(a.lastIndexOf("."));
+		if (b == ".jpeg" || b == ".jpg" || b == ".png" || b == ".gif" || b == ".bmp") {
+			$(this).addClass("bigfa-showbox").attr("id", "bigfa-" + d);
+			d++
+		}
+	});
+	$("#content a.bigfa-showbox").click(function() {
+		var a = $(this).attr("id").split(/bigfa-/)[1],
+		pagesize = bigfa_getPageSize(),
+		bigfa_img_url = $(this).attr("href"),
+		css_bigfa_bg = "z-index:9999;overflow:hidden;position:fixed;left:0;top:0;width:100%;height:100%;background-color:#000;",
+		css_bigfa = "z-index:99999;position:fixed;left:50%;top:50%;",
+		css_bigfa_img = "display:none;border:8px solid #fff;box-shadow:0px 0px 10px rgba(0,0,0,0.6)",
+		css_bigfa_p_n = "display:none;cursor:pointer;position:absolute;top:50%;line-height:80px;margin:-40px 0 0 0;color:#eee;text-shadow:1px 3px 5px #000;font-size:40px;font-family:Arial,Tahoma;";
+		if (typeof document.body.style.maxHeight === "undefined") {
+			alert("\u4E0D\u652F\u6301IE6\uFF01\u8BF7\u4F60\u4EEC\u653E\u8FC7IE6\u5427\uFF0C\u5B83\u592A\u8001\u4E86\uFF0C\u5C31\u8BA9\u5B83\u5B89\u5FC3\u7684\u53BB\u5427\u2026\u2026");
+			return false
+		} else {
+			$("body").append('<div id="bigfa_bg" style="' + css_bigfa_bg + '"></div><p id="zshowbox_loading"></p><div id="bigfa" style="' + css_bigfa + '"><img id="bigfa_img" style="' + css_bigfa_img + '" /><p id="bigfa_prev" style="left:-30px;' + css_bigfa_p_n + '">&laquo;</p><p id="bigfa_next" style="right:-30px;' + css_bigfa_p_n + '">&raquo;</p></div>');
+			$("#bigfa_bg").fadeTo(600, .8);
+			$zshowbox_loading = $("#zshowbox_loading");
+			bigfa_img("#bigfa_img", bigfa_img_url, pagesize, a, d);
+			$("#bigfa_prev,#bigfa_next").click(function() {
+				if ($(this).attr("id") == "bigfa_prev") a--;
+				else a++;
+				$("#bigfa").find("img").remove().end().append('<img id="bigfa_img" style="' + css_bigfa_img + '" />');
+				bigfa_img_url = $("#bigfa-" + a).attr("href");
+				bigfa_img("#bigfa_img", bigfa_img_url, pagesize, a, d);
+				return false
+			});
+			$("#bigfa_bg,#bigfa_img").click(function() {
+				$zshowbox_loading.remove();
+				$("#bigfa_bg,#bigfa_img").unbind("click");
+				$("#bigfa_bg,#bigfa").fadeOut(400,
+				function() {
+					$(this).remove()
+				});
+				return false
+			})
+		}
+		return false
+	});
+	function bigfa_img(b, c, d, e, f) {
+		$("#bigfa_prev,#bigfa_next").hide();
+		var g = new Image();
+		g.onload = function() {
+			var x = d[0] - 100,
+			y = d[1] - 100,
+			img_w = g.width,
+			img_h = g.height;
+			if (img_w > x) {
+				img_h = img_h * (x / img_w);
+				img_w = x;
+				if (img_h > y) {
+					img_w = img_w * (y / img_h);
+					img_h = y
+				}
+			} else if (img_h > y) {
+				img_w = img_w * (y / img_h);
+				img_h = y;
+				if (img_w > x) {
+					img_h = img_h * (x / img_w);
+					img_w = x
+				}
+			}
+			var a = -(img_w / 2 + 5) + "px",
+			margintop = -(img_h / 2 + 5) + "px";
+			img_w = img_w + "px",
+			img_h = img_h + "px";
+			$(b).attr("src", c).css({
+				width: img_w,
+				height: img_h
+			}).fadeIn(600).parent().css({
+				"margin-left": a,
+				"margin-top": margintop
+			});
+			if (e > 0) $("#bigfa_prev").show();
+			if (e < f - 1) $("#bigfa_next").show()
+		};
+		g.src = c
+	}
+	function bigfa_getPageSize() {
+		var a = document.documentElement;
+		var w = window.innerWidth || self.innerWidth || a && a.clientWidth || document.body.clientWidth;
+		var h = window.innerHeight || self.innerHeight || a && a.clientHeight || document.body.clientHeight;
+		arrayPageSize = [w, h];
+		return arrayPageSize
+	}
+});
+
+
